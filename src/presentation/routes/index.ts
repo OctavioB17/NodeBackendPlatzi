fae6ebe4adb04102cd9f2b56b8544758d2f405d2
Octavio Bruza <octaviobruza17@gmail.com>
2025-03-21 15:05:40 -0300

    Initializing routes

--- src/presentation/routes/index.ts
+++ src/presentation/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from 'dotenv';
 import userRouter from "./UserRoutes";
 import productRouter from "./ProductRouter"
+import categoriesRouter from "./CategoriesRouter";
 dotenv.config();
 
 const apiPrefix = process.env.API_PREFIX
@@ -11,6 +12,7 @@ const routerApi = ( app: express.Application ): void => {
   app.use(`${apiPrefix}`, router)
   router.use(`/users`, userRouter)
   router.use(`/products`, productRouter)
+  router.use(`/categories`, categoriesRouter)
 }
 
 export default routerApi
