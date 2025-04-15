a24a32d7e8926b1418ff034e8f2c0b91d0fb7fc3
Octavio Bruza <octaviobruza17@gmail.com>
2025-04-15 16:17:31 -0300

    Trying to fix vercel deploy 2

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { obtainIp } from '../src/utils/functions';
 import app from '../src/server'
 
-export default app
+const port = 3000;
+const ip: string = obtainIp() || 'localhost';
+
+
+app.listen(port, ip, () => {
+  console.log(`Listening in http://${ip}:${port}`)
+})
